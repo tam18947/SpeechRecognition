68b18946a351ca3944e23fe03491bdec236c5f3f
sasaki <sasaki@bio.eng.osaka-u.ac.jp>
2021-06-22 10:46:05 +0900

    dl_bottom

---
+++ dl_bottom.js
@@ -0,0 +1,15 @@
+const SpeechRecognition =
+  window.webkitSpeechRecognition || window.SpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+  document.querySelector("#text").innerHTML += event.results[0][0].transcript + '<br>';
+
+  var element = document.documentElement;
+  var bottom = element.scrollHeight - element.clientHeight;
+  window.scroll(0, bottom);
+};
+
+recognition.onend = () => recognition.start();
+
+recognition.start();
