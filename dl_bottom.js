98ecd68dabbe2c9e63ea3549293a37a3c52aafde
sasaki <sasaki@bio.eng.osaka-u.ac.jp>
2021-06-23 15:21:24 +0900

    Update dl_bottom.js

--- dl_bottom.js
+++ dl_bottom.js
@@ -3,7 +3,7 @@ const SpeechRecognition =
 const recognition = new SpeechRecognition();
 
 recognition.onresult = (event) => {
-  document.querySelector("#text").innerHTML += event.results[0][0].transcript + '<br>';
+  document.querySelector("#text").innerHTML += event.results[0][0].transcript + '。';//'<br>';
 
   var element = document.documentElement;
   var bottom = element.scrollHeight - element.clientHeight;
