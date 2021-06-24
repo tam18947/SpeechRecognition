c775ec0f746292230c8ef9b0e510a4a5385e9333
sasaki <tam18947@gmail.com>
2021-06-24 12:09:27 +0900

    Update dl_bottom1.js

--- dl_bottom1.js
+++ dl_bottom1.js
@@ -17,17 +17,19 @@ recognition.onresult = (event) => {
   var element = document.documentElement;
   if (element.scrollHeight > element.clientHeight)
   {
-    var tmp = '<br>>';
-    //if (sHeight >= element.scrollHeight)
-    while (sHeight >= element.scrollHeight)
+    if (sHeight > element.scrollHeight)
     {
-      document.querySelector("#text").innerHTML = str + event.results[0][0].transcript + tmp;
-      tmp += '<br>>';
-      element = document.documentElement;
+      var tmp = '<br>>';
+      while (sHeight > element.scrollHeight)
+      {
+        document.querySelector("#text").innerHTML = str + event.results[0][0].transcript + tmp;
+        tmp += '<br>>';
+        element = document.documentElement;
+      }
+      tmp = '<br>>';
     }
-    tmp = '<br>>';
+    sHeight = element.scrollHeight;
   }
-  sHeight = element.scrollHeight;
   var bottom = element.scrollHeight - element.clientHeight;
   window.scrollTo({
     top: bottom,
