f52968e8b0c63c461a1eba006cdfe8617223ce1d
sasaki <tam18947@gmail.com>
2021-06-24 13:00:43 +0900

    Update dl_bottom2.js

--- dl_bottom2.js
+++ dl_bottom2.js
@@ -8,20 +8,36 @@ var str = "";
 var sHeight = 0;
 
 recognition.onresult = (event) => {
-  document.querySelector("#text").innerHTML = str + event.results[0][0].transcript;//'。';//'<br>';
+  document.querySelector("#text").innerHTML = str + event.results[0][0].transcript;
   if (event.results[0].isFinal)
   {
     str += event.results[0][0].transcript + '。';
-
-    var element = document.documentElement;
-    var bottom = element.scrollHeight - element.clientHeight;
-    window.scrollTo({
-      top: bottom,
-      behavior: 'smooth',
-    });
   }
+
+  var element = document.documentElement;
+  if (element.scrollHeight > element.clientHeight)
+  {
+    var tmp = '<br>';
+    while (sHeight > element.scrollHeight)
+    {
+      document.querySelector("#text").innerHTML = str + event.results[0][0].transcript + tmp;
+      tmp += '<br>';
+      element = document.documentElement;
+    }
+    sHeight = element.scrollHeight;
+  }
+  var bottom = element.scrollHeight - element.clientHeight;
+  window.scrollTo({
+    top: bottom,
+    behavior: 'smooth',
+  });
 };
 
 recognition.onend = () => recognition.start();
 
 recognition.start();
+
+window.onresize = function () {
+  var element = document.documentElement;
+  sHeight = element.scrollHeight;
+};
