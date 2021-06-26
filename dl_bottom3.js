2b021f4b45e852e1613dab78bde309391e317d8a
ssk <tam18947@gmail.com>
2021-06-26 11:51:04 +0900

    Create dl_bottom3.js

---
+++ dl_bottom3.js
@@ -0,0 +1,43 @@
+const SpeechRecognition =
+  window.webkitSpeechRecognition || window.SpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.interimResults = true;
+recognition.continuous = false;
+var str = "";
+var sHeight = 0;
+
+recognition.onresult = (event) => {
+  document.querySelector("#text").innerHTML = str + event.results[0][0].transcript;
+  if (event.results[0].isFinal)
+  {
+    str += event.results[0][0].transcript + '。<br>';
+  }
+
+  var element = document.documentElement;
+  if (element.scrollHeight > element.clientHeight)
+  {
+    var tmp = '<br>';
+    while (sHeight > element.scrollHeight)
+    {
+      document.querySelector("#text").innerHTML = str + event.results[0][0].transcript + tmp;
+      tmp += '<br>';
+      element = document.documentElement;
+    }
+    sHeight = element.scrollHeight;
+  }
+  var bottom = element.scrollHeight - element.clientHeight;
+  window.scrollTo({
+    top: bottom,
+    behavior: 'smooth',
+  });
+};
+
+recognition.onend = () => recognition.start();
+
+recognition.start();
+
+window.onresize = function () {
+  var element = document.documentElement;
+  sHeight = element.scrollHeight;
+};
