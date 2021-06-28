c26c67e20d55ff0985678aa79420767bbc38ed27
sasaki <tam18947@gmail.com>
2021-06-28 19:53:44 +0900

    Update dl_bottom3.js

--- dl_bottom3.js
+++ dl_bottom3.js
@@ -4,30 +4,30 @@ const recognition = new SpeechRecognition();
 
 recognition.interimResults = true;
 recognition.continuous = false;
-let str = "";
+let txt = "";
 let sHeight = 0;
 let date;
 
 recognition.onresult = (event) => {
+  let str = txt + event.results[0][0].transcript;
   if (event.results[0].isFinal) {
-    str += event.results[0][0].transcript + '。<br>';
-    document.querySelector("#text").innerHTML = str;
+    txt = str + '。<br>';
+    document.querySelector("#text").innerHTML = txt;
   }
   else {
-    document.querySelector("#text").innerHTML = str + event.results[0][0].transcript;
-
-    let element = document.documentElement;
-    if (element.scrollHeight > element.clientHeight) {
-      let tmp = '<br>';
-      while (sHeight > element.scrollHeight) {
-        document.querySelector("#text").innerHTML = str + event.results[0][0].transcript + tmp;
-        tmp += '<br>';
-        element = document.documentElement;
-      }
-      sHeight = element.scrollHeight;
-    }
+    document.querySelector("#text").innerHTML = str;
   }
 
+  let element = document.documentElement;
+  if (element.scrollHeight > element.clientHeight) {
+    let tmp = '<br>';
+    while (sHeight > element.scrollHeight) {
+      document.querySelector("#text").innerHTML = str + tmp;
+      tmp += '<br>';
+      element = document.documentElement;
+    }
+    sHeight = element.scrollHeight;
+  }
   let bottom = element.scrollHeight - element.clientHeight;
   window.scrollTo({
     top: bottom,
@@ -49,7 +49,7 @@ window.onresize = function () {
 
 function timeReset(da) {
   if (date == da) {
-    document.querySelector("#text").innerHTML = "";
-    str = "";
+    txt = "";
+    document.querySelector("#text").innerHTML = txt;
   }
 };
