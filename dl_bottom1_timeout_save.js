1d6c34823f8dda16966c1e4cb5e1669810e160aa
sasaki <tam18947@gmail.com>
2021-06-29 22:47:22 +0900

    Update dl_bottom1_timeout_save.js

--- dl_bottom1_timeout_save.js
+++ dl_bottom1_timeout_save.js
@@ -49,7 +49,7 @@ window.onresize = function () {
 
 function timeReset(da) {
   if (date == da) {
-    download(new Blob([txt]), data + '.txt');
+    download(new Blob([txt]), date + '.txt');
     txt = "";
     document.querySelector("#text").innerHTML = txt;
   }
