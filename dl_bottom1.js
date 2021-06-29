3214925e3a6867dec8e553781b50e15420d01861
sasaki <tam18947@gmail.com>
2021-06-29 13:36:04 +0900

    rename

--- dl_bottom1.js
+++ dl_bottom1.js
@@ -4,29 +4,30 @@ const recognition = new SpeechRecognition();
 
 recognition.interimResults = true;
 recognition.continuous = false;
-var str = "";
-var sHeight = 0;
+let txt = "";
+let sHeight = 0;
 
 recognition.onresult = (event) => {
-  document.querySelector("#text").innerHTML = str + event.results[0][0].transcript;
-  if (event.results[0].isFinal)
-  {
-    str += event.results[0][0].transcript + '。';
+  let str = txt + event.results[0][0].transcript;
+  if (event.results[0].isFinal) {
+    str += '。';
+    txt = str;
   }
+  document.querySelector("#text").innerHTML = str;
 
-  var element = document.documentElement;
-  if (element.scrollHeight > element.clientHeight)
-  {
-    var tmp = '<br>';
-    while (sHeight > element.scrollHeight)
-    {
-      document.querySelector("#text").innerHTML = str + event.results[0][0].transcript + tmp;
+  let element = document.documentElement;
+  if (element.scrollHeight > element.clientHeight) {
+    let tmp = '<br>';
+    while (sHeight > element.scrollHeight) {
+    //for (let i = 0; i < 5 && sHeight > element.scrollHeight; i++) {
+      document.querySelector("#text").innerHTML = str + tmp;
       tmp += '<br>';
       element = document.documentElement;
     }
     sHeight = element.scrollHeight;
   }
-  var bottom = element.scrollHeight - element.clientHeight;
+
+  let bottom = element.scrollHeight - element.clientHeight;
   window.scrollTo({
     top: bottom,
     behavior: 'smooth',
@@ -36,3 +37,8 @@ recognition.onresult = (event) => {
 recognition.onend = () => recognition.start();
 
 recognition.start();
+
+window.onresize = function () {
+  let element = document.documentElement;
+  sHeight = element.scrollHeight;
+};
