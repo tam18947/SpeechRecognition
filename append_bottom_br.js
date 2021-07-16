cd196f96a9d1cf6c19f6515ac417f03dd3a2ab92
sasaki <tam18947@gmail.com>
2021-07-16 16:12:59 +0900

    Create append_bottom_br.js

---
+++ append_bottom_br.js
@@ -0,0 +1,18 @@
+const SpeechRecognition =
+  window.webkitSpeechRecognition || window.SpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+  document.querySelector("#text").innerHTML += event.results[0][0].transcript + '。<br>';
+
+  var element = document.documentElement;
+  var bottom = element.scrollHeight - element.clientHeight;
+  window.scrollTo({
+    top: bottom,
+    behavior: 'smooth',
+  });
+};
+
+recognition.onend = () => recognition.start();
+
+recognition.start();
